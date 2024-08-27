7633afeae3b825ebb772011446c085fb96b320e9
Elissdev <elissandra.dev@gmail.com>
2024-08-26 22:52:43 -0300

    Cypress: Caso de teste 4 - Redefinir senha

--- cypress-testes/cypress.config.js
+++ cypress-testes/cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demo.firefly-iii.org',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });

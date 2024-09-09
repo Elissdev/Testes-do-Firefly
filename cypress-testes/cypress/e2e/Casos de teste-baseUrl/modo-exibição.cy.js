9820573fe5956a0009126fed49a2c2897d523fc5
Elissdev <elissandra.dev@gmail.com>
2024-09-09 14:05:24 -0300

    refatoração dos testes & modo-exibição concluido

--- cypress-testes/cypress/e2e/Casos de teste-baseUrl/modo-exibição.cy.js
+++ cypress-testes/cypress/e2e/Casos de teste-baseUrl/modo-exibição.cy.js
@@ -1,21 +1,46 @@
 describe('Modo de Exibição da Tela', () => {
 
-beforeEach(() => {
-    cy.login();
-    
-  });
+  beforeEach(() => {
+  
+      cy.viewport(1366, 729)
+      // Pré-condição: O usuário deve estar logado no sistema
+      cy.visit('/');  // Acessa a página inicial da aplicação
+  
+      // Insere as credenciais de login
+      cy.get('input[name="email"]').clear().type('demo@firefly');  // Insira o email de login correto
+      cy.get('input[name="password"]').clear().type('demo');  // Insira a senha correta
+      cy.get('button[type="submit"]').click();  // Submete o formulário de login
+  
+    });
+  
 
   it('Deve alterar o modo de exibição para Always dark', () => {
-    // Acesse as preferências
-    cy.contains('Options').click();
-    cy.contains('Preferences').click();
-    cy.contains('Layout').click();
+     cy.contains('Options').click();
+     cy.contains('Preferences').click();
+     cy.contains('Layout').click();
 
-    cy.get('div#layout.tab-pane').click();
+     cy.get('div#layout.tab-pane').click();
+    
+    
+     cy.get('input[name=darkMode][value=dark]').click();
+     cy.get('button[type="submit"]').eq(1).click();
 
-    cy.get('button[type="submit"]').eq(1).click();
+     // Realiza o logout
+     cy.contains('Logout').click(); 
 
-   
+     // Realiza o login novamente
+     cy.get('input[name="email"]').clear().type('demo@firefly');  
+     cy.get('input[name="password"]').clear().type('demo');  
+     cy.get('button[type="submit"]').click();  
+ 
+     // Verifica se o modo "Always dark" persiste
+     cy.contains('Options').click();
+     cy.contains('Preferences').click();
+     cy.contains('Layout').click();
+     
+     cy.get('input[name=darkMode][value=dark]').should('be.checked');
+
   });
-
 });
+
+
--- cypress-testes/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('login', (email = 'demo@firefly', password = 'demo') => {
-    cy.viewport(1366, 729); // Define o tamanho da tela para o teste
-    cy.visit('/'); // Visita a página inicial da aplicação
-  
-    // Insere as credenciais de login
-    cy.get('input[name="email"]').clear().type(email); // Insere o email de login
-    cy.get('input[name="password"]').clear().type(password); // Insere a senha
-    cy.get('button[type="submit"]').click(); // Clica no botão de login
-  });

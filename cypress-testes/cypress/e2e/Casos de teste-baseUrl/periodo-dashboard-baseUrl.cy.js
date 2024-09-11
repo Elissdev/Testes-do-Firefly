06f3599a3e95596b61fbe1f91c1f5272456230b5
Elissdev <elissandra.dev@gmail.com>
2024-09-11 00:29:53 -0300

    Códigos atualizados

--- cypress-testes/cypress/e2e/Casos de teste-baseUrl/periodo-dashboard-baseUrl.cy.js
+++ cypress-testes/cypress/e2e/Casos de teste-baseUrl/periodo-dashboard-baseUrl.cy.js
@@ -2,13 +2,13 @@ describe('Teste: Alteração do Período da Dashboard', () => {
 
   beforeEach(() => {
     cy.viewport(1366, 729)
-      // Pré-condição: O usuário deve estar logado no sistema
-      cy.visit('/');  // Acessa a página inicial da aplicação
-  
-      // Insere as credenciais de login
-      cy.get('input[name="email"]').clear().type('demo@firefly');  // Insira o email de login correto
-      cy.get('input[name="password"]').clear().type('demo');  // Insira a senha correta
-      cy.get('button[type="submit"]').click();  // Submete o formulário de login
+    // Pré-condição: O usuário deve estar logado no sistema
+    cy.visit('/');  // Acessa a página inicial da aplicação
+
+    // Insere as credenciais de login
+    cy.get('input[name="email"]').clear().type('demo@firefly');  // Insira o email de login correto
+    cy.get('input[name="password"]').clear().type('demo');  // Insira a senha correta
+    cy.get('button[type="submit"]').click();  // Submete o formulário de login
     
   });
 

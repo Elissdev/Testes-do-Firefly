d99a03f8e5d6806ddfcbc753a27baf0543b2fdc0
Elissdev <elissandra.dev@gmail.com>
2024-09-11 22:13:22 -0300

    Atualiza configuração do Cypress e ajusta dependências

---
+++ cypress-testes/cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://demo.firefly-iii.org/', // URL base do Firefly III
+  },
+})

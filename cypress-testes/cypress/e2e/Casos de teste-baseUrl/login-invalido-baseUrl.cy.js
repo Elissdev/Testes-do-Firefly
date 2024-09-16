c10f4fffa7af082177ddf07e447b228845a089a5
Elissdev <elissandra.dev@gmail.com>
2024-09-16 20:48:28 -0300

    exclusão dos arquivos duplicados para os arquivos refatorados

--- cypress-testes/cypress/e2e/Casos de teste-baseUrl/login-invalido-baseUrl.cy.js
+++
@@ -1,9 +0,0 @@
-describe('login', () => {
-    it('login invalido', () => {
-      cy.visit('/login')
-      cy.get(':nth-child(2) > .form-control').clear().type('lizzreeis192@gmail.com')
-      cy.get(':nth-child(3) > .form-control').clear().type('123456')
-      cy.get('.btn').click()
-      cy.get('li').should('contain', 'These credentials do not match our records.')
-    });
-  });
